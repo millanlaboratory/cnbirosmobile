a332ab93b14a3d1258c25ed61273adb26c0d3041
Luca Tonin <luca.tonin@gmail.com>
2017-03-03 19:16:15 +0100

    Missing file from previous commit

---
+++ cnbiros_core/include/GridMapTool.hpp
@@ -0,0 +1,23 @@
+#ifndef CNBIROS_CORE_GRIDMAP_TOOL_HPP
+#define CNBIROS_CORE_GRIDMAP_TOOL_HPP
+
+#include <grid_map_ros/grid_map_ros.hpp>
+
+namespace cnbiros {
+	namespace core {
+
+class GridMapTool {
+
+	public:
+		static void ReplaceNaN(grid_map::GridMap& map, float value);	
+		static void Reset(grid_map::GridMap& map, std::string layer, float value = 0.0f);
+		static void Reset(grid_map::GridMap& map, float value = 0.0f);
+		static void SetGeometry(grid_map::GridMap& map, float xsize, float ysize, float res);
+		static void SetFrameId(grid_map::GridMap& map, std::string frame);
+		static grid_map_msgs::GridMap ToMessage(grid_map::GridMap& map);
+};
+	}
+}
+
+
+#endif
---
+++ cnbiros_core/include/RosInterface.hpp
@@ -0,0 +1,153 @@
+#ifndef CNBIROS_CORE_ROSINTERFACE_HPP
+#define CNBIROS_CORE_ROSINTERFACE_HPP
+
+#include <ros/ros.h>
+
+#include "Flags.hpp"
+
+namespace cnbiros {
+	namespace core {
+
+class RosInterface {
+
+	public:
+		//! Constructor
+		RosInterface(void);
+
+		//! Destructor
+		virtual ~RosInterface(void);
+
+		//! Register the interface to a node
+		//! \param node	Pointer to the node	 
+		void Register(ros::NodeHandle* node);
+
+		//! Check if the interface is registered to a node
+		bool IsRegistered(void);
+
+		//! Set interface name 
+		//! \param name Name of the interface
+		void SetName(std::string name);
+
+		//! Get interface name
+		//! \return The interface name
+		std::string GetName(void);
+
+		//! Set interface frequency
+		//! \param frequency Frequency of the interface
+		void SetFrequency(float frequency);
+		
+		//! Get current interface frequency
+		//! \return The interface frequency
+		float GetFrequency(void);
+
+		//! Get an interface subscriber
+		/*! \param topic subscriber topic
+		 *  \return Pointer to the subscriber
+		 */
+		ros::Subscriber* GetSubscriber(std::string topic);
+
+		//! Set a new interface subscriber (with external function as callback)
+		/*! \param topic 	Name of the subscribed topic
+		 *  \param fp 		Callback function to be called when a message
+		 *  				arrives on the subscribed topic
+		 */
+		template<class M>
+		void SetSubscriber(std::string topic, void(*fp)(M));
+		
+		//! Set a new interface subscriber (with class method as callback)
+		/*! \param topic 	Name of the subscribed topic
+		 *  \param fp 		Callback function to be called when a message
+		 *  				arrives on the subscribed topic
+		 *  \param obj		Pointer to the object where the callback is
+		 *  				istantiated
+		 */
+		template<class M, class T>
+		void SetSubscriber(std::string topic, void(T::*fp)(M), T* obj);
+
+		//! Set a new interface publisher
+		/*! \param topic 	Name of the advertise topic
+		 */
+		template<class M>
+		void SetPublisher(std::string topic);
+
+		//! Publish message on advertise topic 
+		/*!  \param msg 		Message to be published
+		 */
+		template<class M>
+		void Publish(M& msg);
+
+		//! Main run method
+		virtual void Run(void) {};
+
+		//! Set stop flag to true
+		//! \sa Resume(), IsStopped()
+		void Stop(void);
+
+		//! Set stop flag to false
+		//! \sa Stop(), IsStopped()
+		void Resume(void);
+
+		//! Check if the stop flag is wheter true or false. This method can be
+		//! used in the main node loop 
+		//! \return Stop flag
+		//! \sa Stop(), Resume()
+		bool IsStopped(void);
+
+	protected:
+		ros::NodeHandle* 	rosnode_;
+		ros::Rate* 			rosrate_;
+		ros::Publisher 		rospub_;
+		std::map<std::string, ros::Subscriber> 	rossubs_;
+
+	private:
+		std::string name_;
+		float 		frequency_;
+		bool 		is_stopped_;
+};
+
+
+template<class M>
+void RosInterface::SetSubscriber(std::string topic, void(*fp)(M)) {
+
+	if(this->IsRegistered()) {
+		this->rossubs_[topic] = this->rosnode_->subscribe(topic, CNBIROS_MESSAGES_BUFFER, fp);
+	} else {
+		ROS_ERROR("[%s] - Cannot subscribe to %s: node is not registered", 
+				  this->GetName().c_str(), topic.c_str());
+	}
+				  
+}
+
+template<class M, class T>
+void RosInterface::SetSubscriber(std::string topic, void(T::*fp)(M), T* obj) {
+	
+	if(this->IsRegistered()) {
+		this->rossubs_[topic] = this->rosnode_->subscribe(topic, CNBIROS_MESSAGES_BUFFER, fp, obj);
+	} else {
+		ROS_ERROR("[%s] - Cannot subscribe to %s: node is not registered", 
+				  this->GetName().c_str(), topic.c_str());
+	}
+}
+
+template<class M>
+void RosInterface::SetPublisher(std::string topic) {
+		
+	if(this->IsRegistered()) {
+		this->rospub_ = this->rosnode_->advertise<M>(topic, CNBIROS_MESSAGES_BUFFER);
+	} else {
+		ROS_ERROR("[%s] - Cannot advertise on %s: node is not registered", 
+				  this->GetName().c_str(), topic.c_str());
+	}
+}
+
+template<class M>
+void RosInterface::Publish(M& msg) {
+	this->rospub_.publish(msg);
+}
+
+
+	}
+}
+
+
+#endif
